060102c415d3233fe0ac0eb3cd4cf9385e4ce43d
OgataiKhan <badellino.kevin@gmail.com>
2023-11-17 09:35:31 +0100

    fix: formatting

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@
 
 // FUNCTIONS
 // Element creator
-function myCreateElement(tag, classNames, content){
+function myCreateElement(tag, classNames, content) {
     const element = document.createElement(tag);
     element.classList.add(...classNames);
     element.append(content);
@@ -23,12 +23,12 @@ function handleCellClick(cell, index) {
     } else {
         cell.classList.add('cell-clicked');
         // Score tracking
-        if(!scoreArray.includes(index)) {
+        if (!scoreArray.includes(index)) {
             scoreArray.push(index);
             score.innerHTML = scoreArray.length;
         }
         // Win condition
-        if(scoreArray.length === (totalCells - totalBombs)) {
+        if (scoreArray.length === (totalCells - totalBombs)) {
             gameOver = true;
             gameEndMessage.innerHTML = `YOU WIN! Your final score is: ${scoreArray.length}`;
         }
@@ -47,7 +47,7 @@ function handlePlayClick() {
     for (let i = 1; i <= totalCells; i++) {
         const myCell = myCreateElement('div', classArray, i);
         // Cell event listener
-        myCell.addEventListener('click', function() {
+        myCell.addEventListener('click', function () {
             handleCellClick(myCell, i);
         });
         // Append cell to fragment
@@ -74,9 +74,9 @@ function setDifficulty(side) {
 // Bomb generator
 function bombGenerator(totalBombsGen, totalCellsGen) {
     bombArray = [];
-    while(bombArray.length < totalBombsGen){
+    while (bombArray.length < totalBombsGen) {
         let bombCellNumber = Math.floor(Math.random() * totalCellsGen) + 1;
-        if(!bombArray.includes(bombCellNumber)) bombArray.push(bombCellNumber);
+        if (!bombArray.includes(bombCellNumber)) bombArray.push(bombCellNumber);
     }
     console.log(bombArray);
     return bombArray;
@@ -108,7 +108,7 @@ let gameOver = false;
 
 // EXECUTION
 // Set grid size and cell number based on selected difficulty
-difficultySelector.addEventListener('change', function() {
+difficultySelector.addEventListener('change', function () {
     const selectedDifficulty = difficultySelector.value;
     switch (selectedDifficulty) {
         case 'easy':
